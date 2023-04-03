55e5e22c6c440f2a5d6e2d625aeb402f9502c56b
vboxuser <vboxuser@OperatingSystem.myguest.virtualbox.org>
2023-04-03 23:15:52 +1000

    Completing test from week 6 fixed repeating issue

--- TCPClient.java
+++ TCPClient.java
@@ -10,19 +10,8 @@ public class TCPClient {
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			DataOutputStream out = new DataOutputStream( s.getOutputStream());
 			
-			// sending the 'HELO' message to server and waiting for reply
-			out.write(("HELO\n").getBytes()); 
-			out.flush();
-			// reading the reply from the server and printing to the command line
-			String data = in.readLine();
-			System.out.println("HELO Received: "+ data);
-			
-			// sending the 'AUTH' with username to server to login
-			out.write(("AUTH vboxuser\n").getBytes());
-			out.flush();
-			data = in.readLine();
-			System.out.println("AUTH Received: "+ data);
-			
+			//Initialise variables
+			String data;
 			boolean flag = true;
 			String LS = null;
 			int LSCount = 0;
@@ -31,37 +20,48 @@ public class TCPClient {
 			int JOBID = 0;
 			String[] REDY = null;
 			
+			// sending the 'HELO' message to server and waiting for reply
+			out.write(("HELO\n").getBytes()); 
+			out.flush();
+			// reading the reply from the server and printing to the command line
+			data = in.readLine();
+			//System.out.println("HELO Received: "+ data);
+			
+			// sending the 'AUTH' with username to server to login
+			out.write(("AUTH vboxuser\n").getBytes());
+			out.flush();
+			data = in.readLine();
+			//System.out.println("AUTH Received: "+ data);
+			
+			
 			// implementing while loop for job dispacher
 			while(data.equals("NONE") == false){
 				// send the 'REDY' message to recieve the information about the next job
 				out.write(("REDY\n").getBytes());
 				out.flush();
 				data = in.readLine();
-				System.out.println("kbjblibReceived: "+ data);
+				
+				System.out.println("Received: "+ data);
 				
 				REDY = data.split(" ");
-				String JOB = REDY[0];				
-				
 				if(flag){
 				// qeries the server state information and stores it in data
 				out.write(("GETS All\n").getBytes());
 				out.flush();
 				data = in.readLine();
-				System.out.println("Received: "+ data);
+				//System.out.println("Received: "+ data);
+				
+				
+				// sends the OK command
+				out.write(("OK\n").getBytes());
+				out.flush();
 				
 				//split the variable data so find the server Info 
 				String[] serverI = data.split(" "); //split the messages by the gaps in the message
 				int nRecs = Integer.parseInt(serverI[1]); //save in nRecs what's after the first gap
 				//System.out.println(nRecs);
 				
-				// sends the OK command
-				out.write(("OK\n").getBytes());
-				out.flush();
 				
-				if(JOB.equals("JCPL")){
-					continue;
-				}
-				else{
 				//start the for loop for the larget server type
 				for(int i = 0; i < nRecs; i++){
 					data = in.readLine();
@@ -71,7 +71,6 @@ public class TCPClient {
 						LS = serverState[0];
 						LSCount = 1;
 						LSCore = Integer.parseInt(serverState[4]);
-						SID = Integer.parseInt(serverState[1]);
 					}
 					
 					if(Integer.parseInt(serverState[4]) == LSCore && serverState[0].equals(LS)){
@@ -85,34 +84,32 @@ public class TCPClient {
 				out.write(("OK\n").getBytes());
 				out.flush();
 				data = in.readLine();
-				System.out.println("Received: "+ data);
+				//System.out.println("Received: "+ data);
 				flag = false;
 				
 				}//end of singular run
-				}
+				
 				
 				
 				
 				// start scheduling jobs
-				if(JOB.equals("JOBN")){
-				
-					JOBID = Integer.parseInt(REDY[2]);
+				if(REDY[0].equals("JOBN")){
+					
+					JOBID = Integer.parseInt(REDY[2]);;
 					
 					out.write(("SCHD "+JOBID+" "+LS+" "+SID+"\n").getBytes());
-					System.out.println(JOBID);
+					//System.out.println(JOBID);
 					out.flush();
 					
 					SID++;
-					if(SID >= LSCount){
+					if(SID >= LSCount-1){
 						SID = 0;
 					}
-					
 					while(data == in.readLine()){
-					continue;
+						continue;
 					}
-				}
 					
-				
+				}
 			}
 			
 			
