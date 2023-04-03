4e68ffbd7128dd909185dfbf490587f630209de8
vboxuser <vboxuser@OperatingSystem.myguest.virtualbox.org>
2023-04-03 17:25:28 +1000

    updates made to start the for loop to find the largest server with most cores

--- TCPClient.java
+++ TCPClient.java
@@ -24,21 +24,55 @@ public class TCPClient {
 			System.out.println("AUTH Received: "+ data);
 			
 			// implementing while loop for job dispacher
-			while(data != "NONE"|| data != "null"){
+			while(data != "NONE"){
 				// send the 'REDY' message to recieve the information about the next job
-				out.write(("REDY").getBytes());
+				out.write(("REDY\n").getBytes());
 				out.flush();
 				data = in.readLine();
 				System.out.println("REDY Received: "+ data);
+				
 				// checking if there are any jobs left to run
-				if(data != "NONE" || data != "null"){
+				if(data != "NONE"){
 					// qeries the server state information and stores it in data
-					out.write(("GETS ALL").getBytes());
+					out.write(("GETS All\n").getBytes());
 					out.flush();
 					data = in.readLine();
 					System.out.println("GETS Received: "+ data);
+					
+					//split the variable data so find the server Info 
+					String[] serverI = data.split(" "); //split the messages by the gaps in the message
+					int nRecs = Integer.parseInt(serverI[1]); //save in nRecs what's after the first gap
+					System.out.println(nRecs);
+					
+					// sends the OK command
+					out.write(("OK\n").getBytes());
+					out.flush();
+					data = in.readLine();
+					System.out.println("OK Received: "+ data);
+					
+					String LS;
+					int LSCount;
+					int LSCore;
+					
+					//start the for loop for the larget server type
+					for(int i = 0; i < nRecs; i++){
+						data = in.readLine();
+						String serverState[] = data.split(" ");
+						
+						if(Integer.parseINT(serverState[4]) > LSCore){
+							LS = serverState[0];
+							LS = 1;
+							LS = Integer.parseInt(serverState[4]);
+						}
+						
+						if(Integer.parseInt(serverState[4]) == LSCore){
+							LSCount++;
+						}
+					}
 				}
 			}
+			
+			
 			out.write(("HELO\n").getBytes()); 
 			out.flush();
 			// reading the reply from the server and printing to the command line
